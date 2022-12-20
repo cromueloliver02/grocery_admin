1d83c031affd03e48e6ccd6f3aac2c92da577f14
cromueloliver02 <cromuelbarut@gmail.com>
2022-12-20 12:36:33 +0800

    created orders page

--- lib/presentation/pages/orders/components/orders_view.dart
+++ lib/presentation/pages/orders/components/orders_view.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
+import '../../../widgets/widgets.dart';
+
 class OrdersView extends StatelessWidget {
   const OrdersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('ORDERS PAGE'),
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: Column(
+        children: [
+          const GCRPageHeader(title: 'ORDERS'),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Text(
+                'All Orders',
+                style: textTheme.headline5,
+              ),
+              const Spacer(),
+              const SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                icon: const Icon(Icons.delete_sweep),
+                label: const Text('Clear'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 20,
+              padding: const EdgeInsets.only(bottom: 20),
+              separatorBuilder: (ctx, idx) => const SizedBox(height: 6),
+              itemBuilder: (ctx, idx) => const GCRProductcard.order(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
