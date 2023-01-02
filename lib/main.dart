328990826c1ce6885b122e160975c57b3838cd2d
cromueloliver02 <cromuelbarut@gmail.com>
2023-01-02 23:52:13 +0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import './utils/utils.dart';
+import './firebase_options.dart';
 import './grocery_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(GroceryApp(
     routerHandler: RouteHandler(),
     blocHandler: BlocHandler(),
